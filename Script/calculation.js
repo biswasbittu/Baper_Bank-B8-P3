b9f4fbbe6678ca6c893bacbfbea362ccf0cb04d4
biswasbittu <biswasbittu7415@yahoo.com>
2024-02-20 22:02:42 +0530

    deposit field done

--- Script/calculation.js
+++ Script/calculation.js
@@ -8,16 +8,21 @@ document
 document.getElementById('btn-deposit').addEventListener('click', function () {
 
     const depositField = document.getElementById('deposit-field').value;
-    
-    console.log(depositField)
+    const currentDeposit = document.getElementById('deposit-current');
+    const dipositTotal = currentDeposit.innerText;
+    // console.log(dipositTotal)
+    // currentDeposit.innerText = depositField;
+    currentDeposit.innerText = parseInt(dipositTotal)+parseInt(depositField);
+    const balenceTotalField = document.getElementById('belance-total');
+    const totalBelance = balenceTotalField.innerText;
+    balenceTotalField.innerText = parseInt(totalBelance) + parseInt(depositField)
 
 
 })
 
 document.getElementById('btn-withdrow').addEventListener('click', function () {
     const withdrowField = document.getElementById('withdraw-field').value;
+
     console.log(withdrowField);
-
-
 
 })
